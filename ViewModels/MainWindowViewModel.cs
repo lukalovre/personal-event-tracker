82168cdc09e1da6a8168e6649b59685ff036c78d
Luka Lovre <lukalovre@protonmail.com>
2023-11-23 20:34:30 +0100

    Added datagrid to control

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,10 @@
-﻿namespace AvaloniaApplication1.ViewModels;
+﻿using System.Collections.Generic;
+
+namespace AvaloniaApplication1.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
 	public string Greeting => "Welcome to Avalonia!";
+
+	public List<string> Movies => new() { "1", "2", "3" };
 }
