ab72c96fb8945a8a7cdb4ec7519f5805530e94c0
Luka Lovre <lukalovre@protonmail.com>
2023-11-27 14:05:03 +0100

    Added MovieViewModel

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -6,15 +6,22 @@ public class MainWindowViewModel : ViewModelBase
 {
     public string Title => "Data";
 
-    public List<Movie> Movies { get; set; }
+    public List<MovieViewModel> Movies { get; set; }
 
     public MainWindowViewModel()
     {
-        Movies = new List<Movie>();
+        Movies = new List<MovieViewModel>();
 
         for (int i = 0; i < 500; i++)
         {
-            Movies.Add(new() { Director = $"Directors {i}" });
+            Movies.Add(
+                new()
+                {
+                    Title = "title",
+                    Director = $"Director {i}",
+                    Year = 2000 + i
+                }
+            );
         }
     }
 }
---
+++ ViewModels/MovieViewModel.cs
@@ -0,0 +1,7 @@
+public class MovieViewModel
+{
+    public string Title { get; set; }
+    public string Director { get; set; }
+
+    public int Year { get; set; }
+}
