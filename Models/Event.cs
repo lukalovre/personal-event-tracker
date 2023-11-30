35cca8ec2e748cf52cf61f0454c27b8c223de740
Luka Lovre <lukalovre@protonmail.com>
2023-11-30 22:39:13 +0100

    adde universal event

--- Models/Event.cs
+++ Models/Event.cs
@@ -1,14 +1,22 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 
-public class Event
+public partial class Event
 {
     [Key]
     public int ID { get; set; }
-    public string ItemID { get; set; }
-    public DateTime? Date { get; set; }
+    public int ItemID { get; set; }
+    public string ExternalID { get; set; }
+    public DateTime? DateStart { get; set; }
+    public DateTime? DateEnd { get; set; }
     public int? Rating { get; set; }
+    public bool Bookmakred { get; set; }
+    public int? Chapter { get; set; }
+    public int Amount { get; set; }
+    public eAmountType AmountType { get; set; }
+    public bool Completed { get; set; }
     public string Comment { get; set; }
     public string People { get; set; }
     public string Platform { get; set; }
+    public int? LocationID { get; set; }
 }
