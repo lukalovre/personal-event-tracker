b0cbfa12a5e39b0087f7d4872914b07754fa96ae
Luka Lovre <lukalovre@protonmail.com>
2023-11-30 22:39:37 +0100

    Adding legacy event structures for conversion

---
+++ Models/Old/BookEvent.cs
@@ -0,0 +1,15 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+public class BookEvent
+{
+    [Key]
+    public int ID { get; set; }
+    public string GoodreadsID { get; set; }
+    public string Rating { get; set; }
+    public string Date { get; set; }
+    public int Pages { get; set; }
+    public bool Read { get; set; }
+    public string Comment { get; set; }
+    public string People { get; set; }
+}
