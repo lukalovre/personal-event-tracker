775b55cdf96e433cb9525059f418f403c33100cf
Luka Lovre <lukalovre@protonmail.com>
2023-12-08 23:58:28 +0100

    Added api keys path

--- Repositories/Paths.cs
+++ Repositories/Paths.cs
@@ -6,9 +6,10 @@ public static class Paths
 {
     private static readonly string _root = "../../Data";
     public static string Images => Path.Combine(_root, "Images");
+    public static string APIKeys => Path.Combine(_root, ".Keys");
     public static string Data => _root;
 
-    public static string GetTempPath<T>() => Path.Combine(_root, "Temp", typeof(T).ToString());
+    public static string GetTempPath<T>() => Path.Combine(_root, ".Temp", typeof(T).ToString());
 
     public static string GetImagesPath<T>() => Path.Combine(Images, typeof(T).ToString());
 
