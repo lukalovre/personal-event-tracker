6ba29ee0d0f35979f28e152a05c21af80a5d41f5
Luka Lovre <lukalovre@protonmail.com>
2023-12-11 10:56:42 +0100

    Creating root if not exist

--- Repositories/Paths.cs
+++ Repositories/Paths.cs
@@ -4,15 +4,27 @@ namespace Repositories;
 
 public static class Paths
 {
-    private static readonly string _root = "../../Data";
-    public static string Images => Path.Combine(_root, "Images");
-    public static string APIKeys => Path.Combine(_root, ".Keys");
-    public static string Data => _root;
+    public static string Images => Path.Combine(GetRootPath(), "Images");
+    public static string APIKeys => Path.Combine(GetRootPath(), ".Keys");
+    public static string Data => GetRootPath();
 
-    public static string GetTempPath<T>() => Path.Combine(_root, ".Temp", typeof(T).ToString());
+    public static string GetTempPath<T>() =>
+        Path.Combine(GetRootPath(), ".Temp", typeof(T).ToString());
 
     public static string GetImagesPath<T>() => Path.Combine(Images, typeof(T).ToString());
 
     public static string GetImagePath<T>(int itemID) =>
         Path.Combine(Images, typeof(T).ToString(), $"{itemID}.png");
+
+    private static string GetRootPath()
+    {
+        var rootPath = "Data";
+
+        if (!Directory.Exists(rootPath))
+        {
+            Directory.CreateDirectory(rootPath);
+        }
+
+        return rootPath;
+    }
 }
