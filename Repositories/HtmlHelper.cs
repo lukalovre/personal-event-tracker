6e719b6bfb1ed4a78caf3857d46dc05bf7946510
Luka Lovre <lukalovre@protonmail.com>
2023-12-15 08:00:31 +0100

    CleanUrl to html helper

--- Repositories/HtmlHelper.cs
+++ Repositories/HtmlHelper.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Text.RegularExpressions;
 
@@ -45,5 +46,10 @@ public static class HtmlHelper
         {
             client.DownloadFile(new Uri(webFile), destinationFile);
         }
+    }
+
+    public static string CleanUrl(string url)
+    {
+        return url.Split('?').FirstOrDefault().Trim();
     }
 }
