44e2af3e073dad93090cf63879ffd93878c465f9
Luka Lovre <lukalovre@protonmail.com>
2023-12-15 06:32:13 +0100

    Moved external repos to external folder

---
+++ Repositories/External/ImdbRepository.cs
@@ -0,0 +1,123 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+public class ImdbRepository
+{
+    private const string API_KEY_FILE_NAME = "omdbapi_key.txt";
+
+    public class ImdbData
+    {
+        public string Actors { get; set; }
+        public string Awards { get; set; }
+        public string BoxOffice { get; set; }
+        public string Country { get; set; }
+        public string Director { get; set; }
+        public string DVD { get; set; }
+        public string Genre { get; set; }
+        public string imdbID { get; set; }
+        public string imdbRating { get; set; }
+        public string imdbVotes { get; set; }
+        public string Language { get; set; }
+        public string Metascore { get; set; }
+        public string Plot { get; set; }
+        public string Poster { get; set; }
+        public string Production { get; set; }
+        public string Rated { get; set; }
+        public string Released { get; set; }
+        public string Response { get; set; }
+        public string Runtime { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public string Website { get; set; }
+        public string Writer { get; set; }
+        public string Year { get; set; }
+    }
+
+    // public static ImdbData GetDataFromAPI(string imdb, bool savePoster = true)
+    // {
+    //     var client = new HttpClient { BaseAddress = new Uri("http://www.omdbapi.com/") };
+
+    //     client.DefaultRequestHeaders.Accept.Add(
+    //         new MediaTypeWithQualityHeaderValue("application/json")
+    //     );
+
+    //     var apiKey = File.ReadAllText(@"..\..\..\Keys\");
+
+    //     var response = client.GetAsync($"?i={imdb}&apikey={apiKey}").Result;
+    //     var imdbData = response.Content.ReadAsAsync<ImdbData>().Result;
+
+    //     client.Dispose();
+
+    //     if (savePoster)
+    //     {
+    //         SavePoster(imdbData);
+    //     }
+
+    //     return imdbData;
+    // }
+
+    // public static TVShow GetTVShow(string url)
+	// 	{
+	// 		string inputImdb = Imdb.GetImdbIDFromUrl(url);
+
+	// 		if (!string.IsNullOrWhiteSpace(url) && string.IsNullOrWhiteSpace(inputImdb))
+	// 		{
+	// 			return GetYoutubeChannel(url);
+	// 		}
+
+	// 		var imdbData = Imdb.GetDataFromAPI(inputImdb);
+
+	// 		var runtime = 0;
+
+	// 		try
+	// 		{
+	// 			runtime = imdbData.Runtime == @"\N" || imdbData.Runtime == @"N/A" ? 0 : int.Parse(imdbData.Runtime.TrimEnd(" min".ToArray()));
+	// 		}
+	// 		catch
+	// 		{
+	// 		}
+
+	// 		return new TVShow
+	// 		{
+	// 			Title = imdbData.Title,
+	// 			Runtime = runtime,
+	// 			Year = int.Parse(imdbData.Year.Split('–').FirstOrDefault()),
+	// 			Imdb = imdbData.imdbID,
+	// 			Actors = imdbData.Actors,
+	// 			Country = imdbData.Country,
+	// 			Director = imdbData.Director,
+	// 			Genre = imdbData.Genre,
+	// 			Language = imdbData.Language,
+	// 			Plot = imdbData.Plot,
+	// 			Type = imdbData.Type,
+	// 			Writer = imdbData.Writer
+	// 		};
+	// 	}
+
+		// private static TVShow GetYoutubeChannel(string url)
+		// {
+		// 	var youtubeData = Links.GetYouTubeChannelNameData(url);
+
+		// 	return new TVShow
+		// 	{
+		// 		Title = youtubeData.Title,
+		// 		Imdb = youtubeData.ID,
+		// 		Year = DateTime.Now.Year,
+		// 		Runtime = 10
+		// 	};
+		// }
+
+    private static void SavePoster(ImdbData imdbData)
+    {
+        // var destinationFile = Path.Combine(Paths.Posters, $"{imdbData.imdbID}");
+        // Web.DownloadPNG(imdbData.Poster, destinationFile);
+    }
+
+    public static string GetImdbIDFromUrl(string url)
+    {
+        return url.Split('/').FirstOrDefault(i => i.StartsWith("tt"));
+    }
+}
