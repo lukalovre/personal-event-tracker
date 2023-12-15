6630a5a1c2f85fb547876bd832636217e3c08ef8
Luka Lovre <lukalovre@protonmail.com>
2023-12-15 06:17:55 +0100

    Fixed time converter

--- ViewModels/Converters/TimeToStringConverter.cs
+++ ViewModels/Converters/TimeToStringConverter.cs
@@ -1,16 +1,24 @@
 using System;
 using System.Globalization;
+using System.Linq;
 using Avalonia.Data.Converters;
+using AvaloniaApplication1.ViewModels.Extensions;
 
 public class TimeToStringConverter : IValueConverter
 {
-    private const string TIME_FORMAT = @"hh\:mm";
-
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        if (value is int time)
+        if (value is int minutes)
         {
-            return TimeSpan.FromMinutes(time).ToString(TIME_FORMAT);
+            var hours = minutes / 60;
+            var min = minutes % 60;
+
+            var hText = hours == 0 ? string.Empty : $"{hours}h";
+            var mText = min == 0 ? string.Empty : $"{min}m";
+
+            var space = hours == 0 || min == 0 ? string.Empty : " ";
+
+            return hText + space + mText;
         }
 
         return null;
@@ -20,12 +28,19 @@ public class TimeToStringConverter : IValueConverter
     {
         if (value is string str)
         {
-            return TimeSpan.ParseExact(
-                str,
-                TIME_FORMAT,
-                CultureInfo.InvariantCulture,
-                TimeSpanStyles.None
-            );
+            var split = str.Split(" ");
+
+            if (split.Length == 1)
+            {
+                return int.Parse(str.TrimEnd("m"));
+            }
+            else
+            {
+                int hours = int.Parse(split[0].TrimEnd("h"));
+                int minutes = int.Parse(split[1].TrimEnd("m"));
+
+                return (hours * 60) + minutes;
+            }
         }
 
         return null;
