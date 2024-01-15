9f2bc99d6c09b49913c974373cbce324017003d9
Luka Lovre <lukalovre@protonmail.com>
2024-01-15 12:44:52 +0100

    Getting Igdb data from html, since api stopped working for some reason

--- Repositories/External/Igdb.cs
+++ Repositories/External/Igdb.cs
@@ -1,6 +1,8 @@
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 using IGDB;
 using Repositories;
 
@@ -15,7 +17,68 @@ public class Igdb : IExternal<Game>
 
     public Game GetItem(string url)
     {
-        return GetDataFromAPIAsync(url).Result;
+
+        using var client = new WebClient();
+        client.Headers.Add("user-agent", "...");
+        var content = client.DownloadData(url);
+        using var stream = new MemoryStream(content);
+        string result = System.Text.Encoding.UTF8.GetString(stream.ToArray());
+        var htmlDocument = new HtmlDocument();
+        htmlDocument.LoadHtml(result);
+
+        var title = GetTitle(htmlDocument);
+        var id = GetID(htmlDocument);
+        var year = GetYear(htmlDocument);
+        var imageUrl = GetImageUrl(htmlDocument);
+
+        var destinationFile = Paths.GetTempPath<Game>();
+        HtmlHelper.DownloadPNG(imageUrl, destinationFile);
+
+        return new Game
+        {
+            Igdb = id,
+            Title = title,
+            Year = year
+        };
+
+        // return GetDataFromAPIAsync(url).Result;
+    }
+
+    private int GetYear(HtmlDocument htmlDocument)
+    {
+        var node = htmlDocument.DocumentNode.SelectSingleNode(
+          "//title");
+
+        var titleAndYear = node.InnerText.Trim();
+
+        var split1 = titleAndYear.Split('(').Last();
+        var split2 = split1.Split(')').First();
+
+        return HtmlHelper.GetYear(split2);
+    }
+
+    private int GetID(HtmlDocument htmlDocument)
+    {
+        var node = htmlDocument.DocumentNode.SelectSingleNode(
+            "//meta[contains(@id, 'pageid')]");
+
+        return int.Parse(node.GetAttributeValue("data-game-id", string.Empty).Trim());
+    }
+
+    private string GetImageUrl(HtmlDocument htmlDocument)
+    {
+        var node = htmlDocument.DocumentNode.SelectSingleNode(
+            "//meta[contains(@property, 'og:image')]");
+
+        return node.GetAttributeValue("content", string.Empty).Trim();
+    }
+
+    private string GetTitle(HtmlDocument htmlDocument)
+    {
+        var node = htmlDocument.DocumentNode.SelectSingleNode(
+            "//meta[contains(@property, 'og:title')]");
+
+        return node.GetAttributeValue("content", string.Empty).Trim();
     }
 
     private IGDBClient GetClient()
