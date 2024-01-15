7364f8ef02ba9ae60a4cb1933af5fd9b6baf9d99
Luka Lovre <lukalovre@protonmail.com>
2024-01-15 07:51:32 +0100

    ChangePathToEevnts

--- Repositories/Paths.cs
+++ Repositories/Paths.cs
@@ -30,7 +30,7 @@ public static class Paths
 
     private static string GetRootPath()
     {
-        var rootPath = Path.Combine(Settings.Instance.DatasourcePath, "Data");
+        var rootPath = Path.Combine(Settings.Instance.DatasourcePath, "Events");
         CreatePath(rootPath);
         return rootPath;
     }
