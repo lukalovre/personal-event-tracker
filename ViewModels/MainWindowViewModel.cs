b2422d090f9666320c684ff4bd1e412493cd06dd
Luka Lovre <lukalovre@protonmail.com>
2024-03-26 14:39:11 +0100

    Adedd zoo

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using AvaloniaApplication1.Models;
-using AvaloniaApplication1.Repositories;
+﻿using AvaloniaApplication1.Repositories;
 using Repositories;
 
 namespace AvaloniaApplication1.ViewModels;
@@ -19,7 +15,7 @@ public class MainWindowViewModel : ViewModelBase
     public TVShowsViewModel TVShowsViewModel { get; }
     public ClipsViewModel ClipsViewModel { get; }
     public SongsViewModel SongsViewModel { get; }
-    public List<ZooGridItem> Zoo { get; set; }
+    public ZooViewModel ZooViewModel { get; }
 
     public MainWindowViewModel(IDatasource datasource)
     {
@@ -33,47 +29,7 @@ public class MainWindowViewModel : ViewModelBase
         TVShowsViewModel = new TVShowsViewModel(datasource, new TVShowExternal());
         ClipsViewModel = new ClipsViewModel(datasource, new ClipsExternal());
         SongsViewModel = new SongsViewModel(datasource, new SongExternal());
-
-        // _datasource.GetEventListConvert<MyWork>();
-        Zoo = GetData<Zoo, ZooGridItem>(datasource);
+        ZooViewModel = new ZooViewModel(datasource);
     }
 
-    private List<T2> GetData<T1, T2>(IDatasource datasource)
-        where T1 : IItem
-        where T2 : class
-    {
-        var itemList = datasource.GetList<T1>();
-        var eventList = datasource.GetEventList<T1>();
-
-        var dateFilter = DateTime.MinValue;
-
-        return eventList
-            .Where(o => o.DateEnd.HasValue && o.DateEnd.Value >= dateFilter)
-            .OrderByDescending(o => o.DateEnd)
-            .DistinctBy(o => o.ItemID)
-            .OrderBy(o => o.DateEnd)
-            .Select(
-                o =>
-                    Convert<T1, T2>(
-                        o,
-                        itemList.First(m => m.ID == o.ItemID),
-                        eventList.Where(e => e.ItemID == o.ItemID)
-                    )
-            )
-            .ToList();
-    }
-
-    private T2 Convert<T1, T2>(Event e, T1 item, IEnumerable<Event> eventList)
-        where T1 : IItem
-        where T2 : class
-    {
-
-        if (typeof(T1) == typeof(Zoo))
-        {
-            var i = item as Zoo;
-            return new ZooGridItem(1, i.Name, i.City, i.Country, e.DateEnd.Value.Year) as T2;
-        }
-
-        return null;
-    }
 }
