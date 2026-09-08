ecb51bb3c115580cbd400a0d33256e4b076633e0
Luka Lovre <lukalovre@protonmail.com>
2026-09-08 15:03:25 +0200

    image zoom

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,7 +1,11 @@
 using System.Collections;
 using System.Linq;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.Layout;
+using Avalonia.Media;
 using Avalonia.Media.Imaging;
 using Avalonia.VisualTree;
 using Avalonia.Controls.Selection;
@@ -20,14 +24,62 @@ public partial class MainWindow : Window
 
     private async void OnButtonClick(object? sender, RoutedEventArgs e)
     {
-        if (e.Source is not Button { Content: Image } button
-            || button.Command is not null
-            || button.DataContext is not IImagePickerViewModel viewModel)
+        if (e.Source is not Button { Content: Image image } button)
         {
             return;
         }
 
-        await viewModel.PickImageAsync(this, button.Tag is "New");
+        if (image.Source is not null)
+        {
+            ShowFullScreenImage(image.Source);
+            return;
+        }
+
+        if (button.DataContext is IImagePickerViewModel viewModel)
+        {
+            await viewModel.PickImageAsync(this, button.Tag is "New");
+        }
+    }
+
+    private void ShowFullScreenImage(IImage source)
+    {
+        var previewWindow = new Window
+        {
+            Title = "Image preview",
+            Width = 1200,
+            Height = 800,
+            WindowStartupLocation = WindowStartupLocation.CenterOwner,
+            ShowInTaskbar = false,
+            CanResize = false,
+            WindowDecorations = global::Avalonia.Controls.WindowDecorations.None,
+            Background = Brushes.Black,
+            Content = new Border
+            {
+                Background = Brushes.Black,
+                Child = new Image
+                {
+                    Source = source,
+                    Stretch = Stretch.Uniform,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center
+                }
+            }
+        };
+
+        previewWindow.WindowState = WindowState.FullScreen;
+
+        previewWindow.KeyDown += (_, keyArgs) =>
+        {
+            if (keyArgs.Key == Key.Escape)
+            {
+                previewWindow.Close();
+            }
+        };
+
+        var imageControl = (Image)((Border)previewWindow.Content!).Child!;
+        imageControl.PointerPressed += (_, _) => previewWindow.Close();
+
+        previewWindow.Show(this);
     }
 
     private void ItemTabControl_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
